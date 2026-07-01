9147249ad34dd9631c8f8a11280434957edc225a
Lee-B-Y <156653892+Lee-B-Y@users.noreply.github.com>
2026-07-01 19:48:23 +0800

    fix: 改善软键盘下的日记编辑体验

--- app/src/main/java/com/lee/vestige/ui/main/MainScreen.kt
+++ app/src/main/java/com/lee/vestige/ui/main/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -328,6 +329,7 @@ private fun EditorScreen(
     BackHandler(onBack = onBack)
 
     Scaffold(
+        modifier = Modifier.imePadding(),
         snackbarHost = { SnackbarHost(snackbarHostState) },
         topBar = {
             TopAppBar(
